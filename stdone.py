0ceb66a743d51674a86cfcf09e550f02269b3134
배윤국 <sleeping_duxk@baeyungug-ui-MacBook-Pro.local>
2020-04-02 16:25:57 +0900

    version2

--- stdone.py
+++ stdone.py
@@ -1,34 +1,49 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://finance.naver.com/item/main.nhn?code=005930'
-req = requests.get(url)
-soup = BeautifulSoup(req.content,'html.parser')
+def get_bs_obj(company_code):
 
-#전일(close)
-first = soup.find("td",{"class":"first"})
-blind = first.find("span",{"class":"blind"})
-close = blind.text
+    url = 'https://finance.naver.com/item/main.nhn?code=' + company_code
+    req = requests.get(url)
+    soup = BeautifulSoup(req.content,'html.parser')
+    return soup
+def get_candle_chart(company_code):
+    soup = get_bs_obj(company_code)
 
-#고가(high)
-table = soup.find("table",{"class":"no_info"})
-trs = table.find_all("tr")
-first_tr = trs[0]
-tds = first_tr.find_all("td")
-second_tds = tds[1]
-high = second_tds.find("span",{"class":"blind"}).text
+    #종목 명
+    name = soup.find("div",{"class":"wrap_company"})
+    nme = name.find("a")
+    event = nme.text
 
-#시가(open)
-second_tr = trs[1]
-tds_second_tr = second_tr.find_all("td")
-first_td_in_second_tr = tds_second_tr[0]
-open = first_td_in_second_tr.find("span",{"class":"blind"}).text
 
-#저가(low)
-second_td_in_second_tr = tds_second_tr[1]
-low = second_td_in_second_tr.find("span",{"class":"blind"}).text
+    #전일(close)
+    first = soup.find("td",{"class":"first"})
+    blind = first.find("span",{"class":"blind"})
+    close = blind.text
 
-print("전일:"+close+"\n")
-print("고가:"+high+"\n")
-print("시가:"+open+"\n")
-print("저가:"+low+"\n")
+    #고가(high)
+    table = soup.find("table",{"class":"no_info"})
+    trs = table.find_all("tr")
+    first_tr = trs[0]
+    tds = first_tr.find_all("td")
+    second_tds = tds[1]
+    high = second_tds.find("span",{"class":"blind"}).text
+
+    #시가(open)
+    second_tr = trs[1]
+    tds_second_tr = second_tr.find_all("td")
+    first_td_in_second_tr = tds_second_tr[0]
+    open = first_td_in_second_tr.find("span",{"class":"blind"}).text
+
+    #저가(low)
+    second_td_in_second_tr = tds_second_tr[1]
+    low = second_td_in_second_tr.find("span",{"class":"blind"}).text
+
+    return{"event":event,"close": close,"high": high,"open": open,"low": low}
+
+company_code = ['028050','047050','096770']
+
+for item in company_code:
+    candle_chart = get_candle_chart(item)
+    print(candle_chart)
+
