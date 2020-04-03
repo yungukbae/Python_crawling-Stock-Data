176ae171627458aa3cd2201aee095215e5a008a5
yungukbae <yungukbae@gmail.com>
2020-04-03 14:59:21 +0900

    summery version

--- NaverAPI.py
+++ NaverAPI.py
@@ -1,18 +1,10 @@
-import os
-import sys
-import urllib.request
-client_id = ""
-client_secret = ""
-encText = urllib.parse.quote("검색할 단어")
-url = "https://openapi.naver.com/v1/search/blog?query=" + encText # json 결과
+import requests
+from urllib.parse import urlparse
+
+keyword = ""
+url = "https://openapi.naver.com/v1/search/blog?query=" + keyword # json 결과
 # url = "https://openapi.naver.com/v1/search/blog.xml?query=" + encText # xml 결과
-request = urllib.request.Request(url)
-request.add_header("X-Naver-Client-Id",client_id)
-request.add_header("X-Naver-Client-Secret",client_secret)
-response = urllib.request.urlopen(request)
-rescode = response.getcode()
-if(rescode==200):
-    response_body = response.read()
-    print(response_body.decode('utf-8'))
-else:
-    print("Error Code:" + rescode)
+result = requests.get(urlparse(url).geturl(),headers = {"X-Naver-Client-Id":"YOUR_CLIENT_ID","X-NAVER-CLIENT-Secret":"YOUR_CLIENT_SECRET"})
+
+json_pbj = result.json()
+print(json_pbj)
